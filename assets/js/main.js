005b9559737fb3838cf6579d0d9344838745f38d
Nguyen Nguyen <michael.nuyennuyen@gmail.com>
2025-03-14 11:53:19 +0700

    contact v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,11 @@ navbarTogglerBtn.addEventListener('click',()=>{
     navbarList.classList.toggle('open');
 })
 
+const contactInputs = document.querySelectorAll('.input-wrapper textarea, .input-wrapper input');
+contactInputs.forEach(contactInput=>{
+    contactInput.addEventListener('blur',(e)=>{
+        const value = e.target.value;
+        if (value !== "")e.target.classList.add('hasValue');
+        else e.target.classList.remove('hasValue');
+    })
+})
